6b371dedfc2dc0e68901cbe0c0d86caf93363310
Jonathan Garson <garsonjonathan22@gmail.com>
2023-11-10 18:29:59 +0100

    add readme, and init.py

---
+++ {{cookiecutter.project_name}}/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='{{ cookiecutter.project_name }}',
+    version='0.1.0',
+    packages=find_packages(),
+    author='{{ cookiecutter.author_name }}',
+    license='{{ cookiecutter.license }}')
